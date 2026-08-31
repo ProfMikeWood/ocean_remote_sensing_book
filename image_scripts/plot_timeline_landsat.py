61e65b44f94d9abe8bec61357831f727b31bfb1d
Mike Wood <133308368+ProfMikeWood@users.noreply.github.com>
2026-08-30 17:47:58 -0700

    adding some landsat pages

--- image_scripts/plot_timeline_landsat.py
+++ image_scripts/plot_timeline_landsat.py
@@ -11,9 +11,9 @@ sat_dates = {'Landsat 1':(np.datetime64('1972-01-01'),np.datetime64('1978-01-21'
              'Landsat 3':(np.datetime64('1978-01-01'),np.datetime64('1983-01-21')),
              'Landsat 4':(np.datetime64('1982-01-01'),np.datetime64('1993-01-21')),
              'Landsat 5':(np.datetime64('1984-01-01'),np.datetime64('2013-01-21')),
-             'Landsat 7':(np.datetime64('1999-01-01'),np.datetime64('2022-01-21')),
-             'Landsat 8':(np.datetime64('2013-01-01'),np.datetime64('2024-12-31')),
-             'Landsat 9':(np.datetime64('2021-01-01'),np.datetime64('2024-12-31'))}
+             'Landsat 7':(np.datetime64('1999-01-01'),np.datetime64('2024-01-19')),
+             'Landsat 8':(np.datetime64('2013-01-01'),np.datetime64('2026-12-31')),
+             'Landsat 9':(np.datetime64('2021-01-01'),np.datetime64('2026-12-31'))}
 sat_names = list(sat_dates.keys())
 
 # make a figure
@@ -33,12 +33,12 @@ for s in range(len(sat_names)):
         plt.text(launch_date - np.timedelta64(4,'W'), s*0.5+0.125, sat_names[s], ha='right', va='center')
 
 # format the axes
-plt.gca().set_xlim([np.datetime64('1972-02-01'),np.datetime64('2025-01-01')])
+plt.gca().set_xlim([np.datetime64('1972-02-01'),np.datetime64('2027-01-01')])
 plt.gca().set_ylim([np.ceil(len(sat_names)/2),-0.25])
 myFmt = mdates.DateFormatter('%Y')
 plt.gca().xaxis.set_major_formatter(myFmt)
 plt.gca().set_yticks([])
-plt.title('Landsat Program Satellites')
+plt.title('Data Acquisition Periods of Landsat Program Satellites')
 plt.grid(linestyle='--', linewidth=0.5, alpha=0.5)
 
 plt.savefig('../images/timeline_landsat.png',bbox_inches='tight')
